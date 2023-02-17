77014c1f356b6f2043fb305b82c95e17fbaab07e
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2023-02-17 13:01:29 +0100

    Update - altered algorithms.

--- Mastermind/mastermind.py
+++ Mastermind/mastermind.py
@@ -1,174 +1,201 @@
-### This code was partially produced by the help of Github Co-pilot and ChatGPT. Other than that, I did not use any other sources.
-
 import random
-import itertools
-import math
 
-def main():
-    print("Welcome to Mastermind!")
-    print("Select a game mode:")
-    print("1. Codemaker (Player creates the code, AI tries to break it)")
-    print("2. Codebreaker (AI creates the code, Player tries to break it)")
-    print("3. Codemaker vs Codebreaker (AI creates the code, AI tries to break it)")
-    game_mode = input("Enter the number corresponding to your choice: ")
-    colors = ["R", "G", "B", "Y", "O", "P"]
-    code_length = 4
-    while True:
-        if game_mode == "1":
-            code = player_codemaker(colors, code_length)
-            game_mode_1(code, colors, code_length)
-        elif game_mode == "2":
-            code = ai_codemaker(colors, code_length)
-            game_mode_2(code, colors)
-        elif game_mode == "3":
-            game_mode_3(colors, code_length)
-        else:
-            print("Invalid choice. Please try again.")
-        play_again = input("Do you want to play again? (yes/no) ").lower()
-        if play_again == "no":
-            break
-        else:
-            game_mode = input("Enter the number corresponding to your choice: ")
-    print("Thanks for playing Mastermind!")
+# Code generator
+def generate_code(colors, code_length):
+    return ''.join([random.choice(colors) for i in range(code_length)])
 
-def player_codemaker(colors, code_length):
-    code = input("Enter your code ({} letters from {}):".format(code_length, colors))
-    while True:
-        if all(c in colors for c in code) and len(code) == code_length:
-            return code
-        else:
-            print("Invalid code. Please try again.")
-            code = input("Enter your code ({} letters from {}):".format(code_length, colors))
+# Code breaker algorithms
+def random_algorithm(colors, code_length):
+    possible_codes = generate_all_possible_codes(colors, code_length)
+    while len(possible_codes) > 1:
+        guess = random.choice(possible_codes)
+        score = get_possible_scores(guess, possible_codes)
+        possible_codes = [code for code in possible_codes if get_possible_scores(code, [guess]) == score]
+    return possible_codes[0]
 
-def ai_codemaker(colors, code_length):
-    code = "".join(random.choices(colors, k=code_length))
-    print("The AI's code is: {}".format(code))
-    return code
-
-def game_mode_1(code, colors, code_length):
-    MAX_GUESSES = 10
-    n = len(colors)
-    s = [set() for i in range(code_length+1)]
-    for x in itertools.product(colors, repeat=code_length):
-        for p in itertools.permutations(x):
-            b, w = get_feedback(p, code)
-            s[b].add(p)
-    guess = random.choice(list(s[code_length-1]))
-    print("The AI's first guess is: {}".format(guess))
-    for i in range(MAX_GUESSES-1):
-        b, w = get_feedback(guess, code)
-        s = [set([x for x in t if get_feedback(x, guess) == (b,w)]) for t in s]
-        if not s[code_length-1]:
-            return []
-        guess = random.choice(list(s[code_length-1]))
-        print("The AI's next guess is: {}".format(guess))
-        print("Steps taken: ", i + 2)
-        if guess == code:
-            print(guess)
-            print("The AI has won!")
-    return [guess]
-
-
-
-def minimax_guess(possible_codes):
-    count = {code: 0 for code in possible_codes}
-    for guess in possible_codes:
+def minimax_algorithm(colors, code_length):
+    possible_codes = generate_all_possible_codes(colors, code_length)
+    guess = random.choice(possible_codes)
+    while len(possible_codes) > 1:
+        max_worst_score = 0
         for code in possible_codes:
-            feedback = get_feedback(guess, code)
-            count[guess] = max(count[guess], min_possible_feedback(feedback, possible_codes))
-    return max(count, key=count.get)
+            worst_score = min([get_possible_scores(guess, [possible_guess])[1] for possible_guess in generate_all_possible_codes(colors, code_length)])
+            if worst_score > max_worst_score:
+                max_worst_score = worst_score
+                guess = code
+        possible_codes = [code for code in possible_codes if get_possible_scores(code, [guess]) == (max_worst_score, 0)]
+    return possible_codes[0]
 
-def update_possible_codes(guess, feedback, possible_codes):
-    return [code for code in possible_codes if get_feedback(guess, code) == feedback]
+def knuth_algorithm(colors, code_length):
+    possible_codes = generate_all_possible_codes(colors, code_length)
+    guess = ''.join([colors[0] for i in range(code_length)])
+    while len(possible_codes) > 1:
+        max_minimax_score = 0
+        for code in possible_codes:
+            minimax_scores = []
+            for possible_guess in generate_all_possible_codes(colors, code_length):
+                minimax_scores.append(get_possible_scores(code, [possible_guess])[1])
+            if min(minimax_scores) > max_minimax_score:
+                max_minimax_score = min(minimax_scores)
+                guess = code
+        possible_codes = [code for code in possible_codes if get_possible_scores(code, [guess]) == (evaluate_guess(code, guess))]
+    return possible_codes[0]
 
-def min_possible_feedback(feedback, possible_codes):
-    min_count = float("inf")
+# Helper functions
+def generate_all_possible_codes(colors, code_length):
+    if code_length == 1:
+        return colors
+    else:
+        return [color + subcode for color in colors for subcode in generate_all_possible_codes(colors, code_length - 1)]
+
+def get_possible_scores(guess, codes):
+    scores = {}
+    for code in codes:
+        score = evaluate_guess(code, guess)
+        if score in scores:
+            scores[score] += 1
+        else:
+            scores[score] = 1
+    return max(scores.items(), key=lambda x: x[1])[0]
+
+def get_secret_code(colors, code_length):
+    print("Enter your secret code:")
+    secret_code = input().upper()
+    while len(secret_code) != code_length or not all(color in colors for color in secret_code):
+        print("Invalid code. Codes must be {} characters long and can only include the colors {}.".format(code_length, colors))
+        secret_code = input().upper()
+    return secret_code
+
+def get_guess_from_algorithm(algorithm, possible_codes, previous_guesses, code_length, colors):
+    scores = {}
     for code in possible_codes:
-        count = 0
-        for other_code in possible_codes:
-            if get_feedback(code, other_code) != feedback:
-                count += 1
-        min_count = min(min_count, count)
-    return min_count
+        score = algorithm(previous_guesses, code, code_length, colors)
+        if score in scores:
+            scores[score].append(code)
+        else:
+            scores[score] = [code]
 
+    # Choose the code with the minimum worst-case scenario
+    max_min_count = 0
+    best_code = None
+    for code, code_set in scores.items():
+        code_set_count = len(code_set)
+        if code_set_count > max_min_count:
+            max_min_count = code_set_count
+            best_code = code_set[0]
+        elif code_set_count == max_min_count:
+            best_code = code_set[0]
 
-def ai_guesser(colors, code_length, feedback=None):
-    if feedback:
-        # Use feedback to eliminate possibilities and make a better guess
-        pass
-    else:
-        # Make an initial random guess
-        guess = "".join(random.choice(colors) for _ in range(code_length))
-    return guess
+    return best_code
 
-def game_mode_2(code, colors):
-    code_length = len(code)
-    print("The code has been created. You have 10 chances to guess the code.")
-    for i in range(10):
-        guess = input("Enter your guess ({} letters from {}):".format(code_length, colors))
-        while True:
-            if all(c in colors for c in guess) and len(guess) == code_length:
-                break
-            else:
-                print("Invalid guess. Please try again.")
-                guess = input("Enter your guess ({} letters from {}):".format(code_length, colors))
-        feedback = get_feedback(guess, code)
-        print("{} black pegs and {} white pegs".format(feedback[0], feedback[1]))
-        if feedback[0] == code_length:
-            print("You win! The code was {}".format(code))
-            break
-    else:
-        print("You lose! The code was {}".format(code))
-
-
-def ai_guess(code, colors):
-    guess = "".join(random.choices(colors, k=len(code)))
-    return guess
-
-def get_feedback(guess, code):
+# Feedback generator
+def evaluate_guess(secret_code, guess):
     black_pegs = 0
     white_pegs = 0
-    code_list = list(code)
-    for i in range(len(guess)):
-        if guess[i] == code[i]:
+    code_freq = {}
+    guess_freq = {}
+
+    for i in range(len(secret_code)):
+        if secret_code[i] == guess[i]:
             black_pegs += 1
-            code_list[i] = None
-    for i in range(len(guess)):
-        for j in range(len(code_list)):
-            if guess[i] == code_list[j]:
-                white_pegs += 1
-                code_list[j] = None
-                break
-    return black_pegs, white_pegs - black_pegs
+        else:
+            if secret_code[i] in guess_freq:
+                guess_freq[secret_code[i]] += 1
+            else:
+                guess_freq[secret_code[i]] = 1
 
+            if guess[i] in code_freq:
+                code_freq[guess[i]] += 1
+            else:
+                code_freq[guess[i]] = 1
 
-def original_algorithm(code, colors, code_length):
-    possible_codes = [''.join(i) for i in itertools.product(colors, repeat=code_length)]
-    guess = random.choice(possible_codes)
-    for i in range(1, len(possible_codes)):
-        feedback = get_feedback(code, guess)
-        possible_codes = [x for x in possible_codes if get_feedback(x, guess) == feedback]
-        if len(possible_codes) == 4:
-            return possible_codes[0]
-        guess = random.choice(possible_codes)
-    return guess
+    for color in code_freq:
+        if color in guess_freq:
+            white_pegs += min(code_freq[color], guess_freq[color])
 
-def game_mode_3(colors, code_length):
-    code = ai_codemaker(colors, code_length)
-    possible_codes = [''.join(i) for i in itertools.product(colors, repeat=code_length)]
-    guess = original_algorithm(code, colors, code_length)
-    turn = 0
-    while True:
-        turn += 1
-        print("AI's guess: ", guess)
-        feedback = get_feedback(code, guess)
-        print("Feedback: ", feedback)
-        if feedback == code_length * 'B':
-            print("AI broke the code in {} turns".format(turn))
+    return black_pegs, white_pegs
+
+def play_game_1():
+    colors = ['R', 'G', 'B', 'Y', 'O', 'P']
+    code_length = 4
+    max_guesses = 10
+    secret_code = generate_code(colors, code_length)
+    print(f"The secret code is: {secret_code}")
+    guesses = []
+    for i in range(max_guesses):
+        print(f"Guess #{i+1}:")
+        guess = input().upper()
+        while len(guess) != code_length or not all(color in colors for color in guess):
+            print(f"Invalid guess. Guesses must be {code_length} characters long and can only include the colors {colors}.")
+            guess = input().upper()
+        guesses.append(guess)
+        if guess == secret_code:
+            print(f"Congratulations! You correctly guessed the code {secret_code} in {len(guesses)} guesses.")
             break
-        code = ai_codemaker(colors, code_length)
-        possible_codes = [x for x in possible_codes if get_feedback(x, guess) == feedback]
-        guess = original_algorithm(code, colors, code_length)
+        else:
+            score = evaluate_guess(guess, secret_code)
+            print(f"Result: {score}")
+            if i == max_guesses - 1:
+                print(f"Sorry, you did not guess the code within {max_guesses} guesses. The secret code was {secret_code}.")
 
-if __name__ == '__main__':
-    main()
+def play_game():
+    print("Welcome to Mastermind!")
+    print("Enter 'q' at any time to quit the game.")
+
+    colors = ["R", "O", "Y", "G", "B", "P"]
+    code_length = 4
+
+    while True:
+        print("\nDo you want to be the codebreaker (guess the code) or the codemaker (have the computer guess your code)?")
+        print("Enter '1' to be the codebreaker, '2' to be the codemaker.")
+        choice = input("Your choice: ")
+        if choice == "q":
+            break
+        elif choice == "1":
+            secret_code = generate_code(colors, code_length)
+            print("The computer has generated a secret code for you to guess.")
+            print("The code consists of {} colors, and the colors can repeat.".format(code_length))
+            print("The possible colors are:", ", ".join(colors))
+            break
+        elif choice == "2":
+            print("You will generate a secret code, and the computer will try to guess it.")
+            print("The code consists of {} colors, and the colors can repeat.".format(code_length))
+            print("The possible colors are:", ", ".join(colors))
+            secret_code = get_secret_code(colors, code_length)
+            guess_algorithm = input("Which guess algorithm do you want to use? Enter '1', '2', or '3': ")
+            break
+        else:
+            print("Invalid choice. Please enter '1' or '2'.")
+
+    guesses = []
+    scores = []
+    num_guesses = 0
+
+    while True:
+        num_guesses += 1
+        print("\nGuess #{}:".format(num_guesses))
+
+        if choice == "1":
+            play_game_1()
+        else:
+            guess = get_guess_from_algorithm(guesses, scores, colors, code_length, guess_algorithm)
+
+        if guess == "q":
+            print("The secret code was:", ", ".join(secret_code))
+            return
+
+        score = evaluate_guess(guess, secret_code)
+        guesses.append(guess)
+        scores.append(score)
+
+        if score == (code_length, 0):
+            print("Congratulations, you guessed the code in {} guesses!".format(num_guesses))
+            print("The secret code was:", ", ".join(secret_code))
+            return
+
+        print("Score:", score)
+
+
+if __name__ == "__main__":
+    play_game()
+
