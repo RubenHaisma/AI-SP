bc8a77cb1b19f03ac47b551dae2b84a050aa6852
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2023-03-25 12:56:50 +0100

    Big edit
    Dropped main, altered sql for recommemndation and big changes to content filtering

--- Buisness rules/content_filtering.py
+++ Buisness rules/content_filtering.py
@@ -14,63 +14,34 @@ connection = psycopg2.connect(user="postgres",
 cursor = connection.cursor() 
 
 # This script will create a table with content recommendations for each category
-def content_recommendations():
-    sql = """SELECT p.id,p.name,p.category,p.sub_category,p.sub_sub_category, COUNT (cart.product_id) AS WOW
+def content_filtering():
+    sql = """SELECT p.id,p.category,p.sub_category,p.sub_sub_category
             FROM product AS p
-            FULL OUTER JOIN cart ON p.id = cart.product_id
-            GROUP BY p.id,p.name,p.category,p.sub_category,p.sub_sub_category,cart.bought
-            ORDER BY p.category,p.sub_category,p.sub_sub_category, COUNT(cart.product_id) DESC"""
+            GROUP BY p.id,p.category,p.sub_category,p.sub_sub_category
+            ORDER BY p.category,p.sub_category,p.sub_sub_category"""
     cursor.execute(sql)
-    similarproduct = cursor.fetchall()
+    similarProducts = cursor.fetchall()
     recommendList = []
-    for product in similarproduct:
-        if product[4]: #category
+    for product in similarProducts:
+        print(product)
+        if len(product) > 4 and product[4]: #category
             category = product[4]
-            if product[3]: #sub_category
+            if len(product) > 3 and product[3]: #sub_category
                 category = product[3]
-                if product[2]: #sub_sub_category
+                if len(product) > 2 and product[2]: #sub_sub_category
                     category = product[2]
                     recommendList.append(product[0])
 
+
         if len(recommendList) == 4:
-            # String manipulation
-            listString = str(recommendList).replace('[','{').replace(']','}').replace('\'','"')
             insertSql = f"""INSERT INTO content_recommendations 
                 (category,product_recommendation)
-                VALUES ('{category}','{listString}')"""
+                VALUES ('{category}')"""
             recommendList = []
             cursor.execute(insertSql)
 
-def collab_recommendations(segmentList,targetAudienceList):
-    for segment in segmentList:
-        for target in targetAudienceList:
-            sql = f"""SELECT DISTINCT cart.product_id, cart.sessions_profiles_id, sessions.segment FROM cart
-            INNER JOIN sessions ON cart.sessions_profiles_id = sessions.browser_id
-            WHERE sessions.segment = '{segment}' 
-            -- AND sessions.target_audience = '{target}'
-            AND sessions.segment IS NOT NULL
-            ORDER BY cart.product_id ASC"""
-            cursor.execute(sql)
-            allSimilarBuyersAndproduct = cursor.fetchall()
-            used = []
-            recommendList = []
-            for buyerTypeAndProduct in allSimilarBuyersAndproduct:
-                if buyerTypeAndProduct[0] in used:
-                    continue
-                else:
-                    used.append(buyerTypeAndProduct[0])
-                    recommendList.append(buyerTypeAndProduct[0])
-                    if len(recommendList) == 4:
-                        # String manipulation
-                        listString = str(recommendList).replace('[','{').replace(']','}').replace('\'','"')
-                        insertSql = f"""INSERT INTO collab_recommendations 
-                            (segment,target_audience,product_recommendation)
-                            VALUES ('{segment}','{target}','{listString}')"""
-                        cursor.execute(insertSql)
-                        recommendList = []
-
 def run():
-    content_recommendations()
+    content_filtering()
     connection.commit()
     cursor.close()
     connection.close()
