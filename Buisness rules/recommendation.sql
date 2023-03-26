d0e4f24f172d9aa69466e152b7bd7cb2232b8dbf
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2023-03-26 19:04:04 +0200

    Update recommendation
    Deleted and modified some lines

--- Buisness rules/recommendation.sql
+++ Buisness rules/recommendation.sql
@@ -3,5 +3,13 @@ DROP TABLE IF EXISTS content_recommendations CASCADE;
 CREATE TABLE content_recommendations (
     id SERIAL PRIMARY KEY,
     category text NOT NULL,
-    product_recommendation text[] NOT NULL
+    product_recommendation VARCHAR(255)[]
+);
+
+DROP TABLE IF EXISTS collab_recommendations CASCADE;
+
+CREATE TABLE collab_recommendations (
+    product_id varchar(255) NOT NULL,
+    product_recommendation text,
+    PRIMARY KEY (product_id)
 );
